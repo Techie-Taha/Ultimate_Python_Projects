576239033c549f8f4a5d7e7c93d639462ba08ee2
Taha <ta753090@gmail.com>
2025-08-21 01:07:15 -0400

    File added

--- Project 2, Rock Paper Scissors/rps.py
+++ Project 2, Rock Paper Scissors/rps.py
@@ -6,7 +6,7 @@
 import random
 computer = random.choice([1, 2, 3])
 choice = input("Enter your choice: ")
-youDict = {"r": 1, "p":2, "s": 3}
+youDict = {"r": 1, "p": 2, "s": 3}
 reverseDict = {1: "Rock", 2: "Paper", 3: "Scissor"}
 
 you = youDict[choice]
@@ -16,17 +16,17 @@ if(computer == you):
     print("It's a draw, try again!")
     # we gonna write a nested for loop
 else:
-    if(computer ==1 and you ==2):
+    if(computer ==1 and you == 2):
         print("you win")
     elif(computer == 1 and you == 3):
         print("you lose")
-    elif(computer ==2 and you ==1):
+    elif(computer ==2 and you == 1):
         print("you lose")
-    elif(computer ==2 and you ==3):
+    elif(computer ==2 and you == 3):
         print("you win")
-    elif(computer ==3 and you ==1):
+    elif(computer ==3 and you == 1):
         print("you win")
-    elif(computer ==3 and you ==2):
+    elif(computer ==3 and you == 2):
         print("you lose")
     else:
         print("Error")
